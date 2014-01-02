9fa0a4a9f0dff80b3985e964094d5506c9e33384
vzvenyach <vzvenyach@dccouncil.us>
2014-01-02 03:22:45 -0500

    updated index

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@
 
 exports.index = function(req, res){
 //  res.render('index', { title: 'Dave\'s test server' });
-  res.send("<h1>Effective Date Counter</h1><p>To use it, change the URL above to '/effdate?t=' followed by the date, with the format '2013-01-01'</p>");
+  res.send("<h1>Effective Date Counter</h1><p>To use it, use the form below:</p><form action='effdate' method='get'>Transmittal Date: [format '2013-01-01']<input type='text' name='t'><br><input type='submit' value='Submit'></form>");
 };
