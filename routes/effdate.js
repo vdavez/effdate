b612f30e22bc6d97ef9261271bea9c89a1dc6d14
vzvenyach <vdavez@gmail.com>
2014-02-05 23:46:47 -0500

    late night fixes

--- routes/effdate.js
+++ routes/effdate.js
@@ -44,7 +44,7 @@ exports.api = function(req, res){
 	out = getEffDate(t, dayscount, p);
 	console.log("Effective Date: " + out[1]);
 	console.dir("Count: " + out[0]);
-	var jsonObj = {"transmittal": moment(t).format("YYYY-MM-DD"), "effdate":out[1].format("YYYY-MM-DD"), "days_array":out[0], "effdate_long": out[1].format("dddd, MMM. DD, YYYY"), "criminal":(dayscount == "60" ? true : false), "prediction":(prediction == "yes" ? true : false)};
+	var jsonObj = {"transmittal": moment(t).format("YYYY-MM-DD"), "effdate":out[1].format("YYYY-MM-DD"), "days_array":out[0], "effdate_long": out[1].format("dddd, MMM DD, YYYY"), "criminal":(dayscount == "60" ? true : false), "prediction":(p == "yes" ? true : false)};
 	res.json(jsonObj);
 }
 
