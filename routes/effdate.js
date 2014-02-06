702a8c6a0df32123cf82671241ae3b5434c02d54
vzvenyach <vdavez@gmail.com>
2014-02-05 23:11:51 -0500

    naive predictor api built

--- routes/effdate.js
+++ routes/effdate.js
@@ -40,7 +40,9 @@ exports.api = function(req, res){
 	var dayscount = req.query.c;
 	if (dayscount == undefined) dayscount = 30;
 	var t = req.query.t;
-	out = getEffDate(t, dayscount);
+	var p = req.query.p;
+	if (p == undefined) p = "no";
+	out = getEffDate(t, dayscount, p);
 	console.log("Effective Date: " + out[1]);
 	console.dir("Count: " + out[0]);
 	var jsonObj = {"transmittal": moment(t).format("YYYY-MM-DD"), "effdate":out[1].format("YYYY-MM-DD"), "days_array":out[0], "effdate_long": out[1].format("dddd, MMM. DD, YYYY"), "criminal":(dayscount == "60" ? true : false) };
@@ -48,20 +50,25 @@ exports.api = function(req, res){
 }
 
 
-function getEffDate(transmittal, dayscount) {
+function getEffDate(transmittal, dayscount, prediction) {
 var out = [];
 var i = 0;
 var house = JSON.parse(fs.readFileSync('./public/house.json','utf-8')).house;
 var senate = JSON.parse(fs.readFileSync('./public/senate.json','utf-8')).senate;    
 var sDays = _.union(house, senate);
-var _recess = fs.readFileSync('./public/recess_days.json','utf-8');
+var _recess = JSON.parse(fs.readFileSync('./public/recess_days.json','utf-8'));
+
+if (prediction == "yes") {
+	_recess.push({"begin":"2014/02/17","end":"2014/02/21"}, {"begin":"2014/03/17","end":"2014/03/21"}, {"begin":"2014/04/14","end":"2014/04/25"}, {"begin":"2014/08/04","end":"2014/09/05"})
+}
+
 var dayOne = moment(transmittal);
 if (!dayOne.isValid()) {return out = ['<a href="./">Try again with a Valid Date</a>', 'Invalid Date Entered']}
 else {
 var c = moment(dayOne);
 while (i < dayscount) {
 //Count the day?
-    if (isCountedDay(sDays,c)) {out[i] = c.format("YYYY-MM-DD"); i++;}
+    if (isCountedDay(sDays,c, _recess)) {out[i] = c.format("YYYY-MM-DD"); i++;}
     c = getNextDay(sDays,c);
 }
 }
@@ -80,9 +87,9 @@ function getNextSessionDay (arr, val) {
 	}
 }
 
-function isCountedDay (arr, val) {
+function isCountedDay (arr, val, r) {
     //Check for count
-    if (val.weekday() == 0 || val.weekday() == 6 || typeof(val.holiday()) != "undefined" || inRecess(val)) {return false}
+    if (val.weekday() == 0 || val.weekday() == 6 || typeof(val.holiday()) != "undefined" || inRecess(val, r)) {return false}
     else {return true}
 }
 
@@ -103,8 +110,9 @@ function getNextDay(arr,val) {
 	return nextDay;
 }
 
-function inRecess (val) {
+function inRecess (val, r) {
 	//iterate to check if in range
+	if (r != undefined) _recess = r;
     val = val.format("YYYY/MM/DD");
     for (var i = 0; i < _recess.length; i++) {
         var begin = _recess[i].begin;
