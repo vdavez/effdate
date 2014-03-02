81f430ed3bb100bbdde023e558361cad23d41ab3
vzvenyach <vdavez@gmail.com>
2014-03-02 09:49:07 -0500

    refactor

--- app.js
+++ app.js
@@ -5,12 +5,13 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var _ = require('underscore');
 var effdate = require('./routes/effdate');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var knox = require('knox');
+var cronJob = require('cron').CronJob;
 
 var app = express();
 var cons = require('consolidate');
@@ -33,25 +34,22 @@ if ('development' == app.get('env')) {
 }
 
 // OnFirstLoad, you get the latest from Amazon
-var files = ['public/house.json','public/senate.json','public/recess_days.json'];
-_.each(files, function (f) {
-	var file = fs.createWriteStream('./' + f);
-	http.get('http://s3-us-west-2.amazonaws.com/effdate/' + f, function(res) {
-    	res.on('data', function(data) {
-            file.write(data);
-        }).on('end', function() {
-            file.end();
-        });
-    });
-	console.log('file: ' + f + ' = successfully loaded');
-});
+var sessionBuilder = require('./sessionBuilder');
+var recessBuilder = require('./recessBuilder');
+
+new cronJob('00 00 * * * *', function () {
+  sessionBuilder;
+}, null, true, "America/Los_Angeles");
+
+new cronJob('00 10 * * * *', function () {
+  recessBuilder;  
+}, null, true, "America/Los_Angeles");
 
 // Registering underscore template
 app.engine('html', cons.underscore);
 app.set('view engine', 'html');
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 app.get('/effdate', effdate.effdate);
 app.get('/api/1/', effdate.api);
 
